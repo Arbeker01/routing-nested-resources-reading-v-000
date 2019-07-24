441e0acf6784488b161ce65eed8d2df47d3ea60e
Romeo Gono <romeogono1@aol.com>
2019-07-24 11:03:13 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -4,16 +4,4 @@ class AuthorsController < ApplicationController
     @author = Author.find(params[:id])
   end
 
-  def posts_index
-    @author = Author.find(params[:id])
-    @posts = Author.posts
-    render template: 'posts/index'
-  end
-
-  def post
-    @author = Author.find(params[:id])
-    @post = Post.find([:post_id])
-    render template: 'posts/show'
-  end
-
 end
